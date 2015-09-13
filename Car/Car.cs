ba82f008448f0834abf7fdb1816fbdcb5226b163
Stefan Mehnert <StefanMehnert@gmail.com>
2015-09-13 09:50:05 +0200

    Added additional Required Projects

---
+++ Car/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Car
+    {
+        
+        public void Start()
+        {
+            Debug.WriteLine("Car.Start()");
+        }
+    }
+}
