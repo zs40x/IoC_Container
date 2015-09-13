eb8e97315365715c4f6d04c5c46c43677ac974ca
Stefan Mehnert <StefanMehnert@gmail.com>
2015-09-13 09:58:10 +0200

    Implemented First Test for Default Constructor Instantiation

---
+++ An_IoC_Container_Test/ResolverTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using An_IoC_Container;
+using An_IoC_Container_Test.Support;
+
+namespace An_IoC_Container_Test
+{
+    [TestClass]
+    public class ResolverTest
+    {
+        private Resolver fcut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            fcut = new Resolver();
+        }
+
+        [TestMethod]
+        public void ResolvesASimpleClassWithDefaultConstructor()
+        {
+            fcut.Register(typeof(IAnInterface), typeof(IASimpleClassWithDefaultConstructor));
+
+            object resolvedInstance = fcut.Resolve(typeof(IAnInterface));
+
+            Assert.IsNotNull(resolvedInstance);
+
+            Assert.IsInstanceOfType(resolvedInstance, typeof(IASimpleClassWithDefaultConstructor));
+        }
+    }
+}
