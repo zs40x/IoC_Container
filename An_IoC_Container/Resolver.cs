78240bda4eda8c539450f3442e92fd35043fab02
Stefan Mehnert <StefanMehnert@gmail.com>
2015-11-15 06:48:56 +0100

    Added Resolver.cs to Repo

---
+++ An_IoC_Container/Resolver.cs
@@ -0,0 +1,45 @@
+﻿using An_IoC_Container.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace An_IoC_Container
+{
+    public class Resolver
+    {
+        private Dictionary<Type, Type> typeMap = new Dictionary<Type, Type>();
+
+        public T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        public object Resolve(Type typeToResolve)
+        {
+            var typeToInstantiate =
+                typeMap.Where(t => t.Key == typeToResolve).FirstOrDefault().Value;
+
+            if (typeToInstantiate == null)
+                throw new TypeNotRegisteredException("Type " + typeToResolve.ToString() + " is not registered in the IoC Container");
+
+            var constructor = typeToInstantiate.GetConstructors().First();
+            var constructorParameters = constructor.GetParameters();
+
+            if(constructorParameters.Count() == 0)
+                return Activator.CreateInstance(typeToInstantiate);
+
+            var dependencyInstances = new List<object>();
+            foreach(var parameter in constructorParameters)
+                dependencyInstances.Add(Resolve(parameter.ParameterType));
+
+            return constructor.Invoke(dependencyInstances.ToArray());
+        }
+
+        public void Register(Type t1, Type t2)
+        {
+            typeMap.Add(t1, t2);
+        }
+    }
+}
